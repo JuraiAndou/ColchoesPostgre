6d433bad92cfbce0545a0cee1e5880e6c3acbd67
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2021-08-26 22:51:40 -0300

    Update main.py
    adição de comentários

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from clienteDAO import ClienteDAO
 
+#variáveis para conexão com o banco de dados
 pg_user = "postgres" #usuário do postgre
 pg_password = "root" #senha do servidor
 pg_port="5432" #porta do servidor
@@ -8,7 +9,7 @@ pg_database = "venda_colchoes" #nome do banco de dados
 def main():
     menu_listar_todos_clientes()
 
-def menu_listar_todos_clientes():
+def menu_listar_todos_clientes():#lista todos os clientes cadastrados
     clienteDAO = ClienteDAO(pg_user, pg_password, pg_port, pg_database)
     clientes = clienteDAO.lista_todas()
     for c in clientes:
