5c53ff8d2b8ace4d27bb903091a70b0ed47a3a9c
L0nelyB0y <rianvictortri@hotmail.com>
2021-08-29 00:26:59 -0300

    att banco certo

---
+++ backupDB/venda_colchoes_esquema.sql
@@ -0,0 +1,339 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.3
+-- Dumped by pg_dump version 13.3
+
+-- Started on 2021-08-29 00:22:47
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 200 (class 1259 OID 17088)
+-- Name: cliente; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.cliente (
+    cpf bigint NOT NULL,
+    nome character varying,
+    email character varying,
+    endereco character varying
+);
+
+
+ALTER TABLE public.cliente OWNER TO postgres;
+
+--
+-- TOC entry 201 (class 1259 OID 17094)
+-- Name: estoque; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.estoque (
+    filial_id integer NOT NULL,
+    produto_id integer NOT NULL,
+    quantidade integer
+);
+
+
+ALTER TABLE public.estoque OWNER TO postgres;
+
+--
+-- TOC entry 202 (class 1259 OID 17097)
+-- Name: filiais; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.filiais (
+    id integer NOT NULL,
+    endereco character varying,
+    email character varying,
+    nome character varying
+);
+
+
+ALTER TABLE public.filiais OWNER TO postgres;
+
+--
+-- TOC entry 203 (class 1259 OID 17103)
+-- Name: fornece; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.fornece (
+    filial_id integer NOT NULL,
+    fornecedor_id integer NOT NULL,
+    quantidade integer,
+    data character varying
+);
+
+
+ALTER TABLE public.fornece OWNER TO postgres;
+
+--
+-- TOC entry 204 (class 1259 OID 17109)
+-- Name: fornecedor; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.fornecedor (
+    id integer NOT NULL,
+    "DDD" integer,
+    numero integer,
+    email character varying,
+    endereco character varying
+);
+
+
+ALTER TABLE public.fornecedor OWNER TO postgres;
+
+--
+-- TOC entry 205 (class 1259 OID 17115)
+-- Name: funcionario; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.funcionario (
+    cpf bigint NOT NULL,
+    nome character varying,
+    cargo character varying,
+    salario integer
+);
+
+
+ALTER TABLE public.funcionario OWNER TO postgres;
+
+--
+-- TOC entry 206 (class 1259 OID 17121)
+-- Name: produto; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.produto (
+    id integer NOT NULL,
+    nome character varying,
+    marca character varying,
+    descricao character varying,
+    fornecedor_id integer,
+    medida character varying,
+    valor integer
+);
+
+
+ALTER TABLE public.produto OWNER TO postgres;
+
+--
+-- TOC entry 207 (class 1259 OID 17127)
+-- Name: venda; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.venda (
+    id integer NOT NULL,
+    filial_id integer,
+    cliente_cpf bigint,
+    funcionario_cpf bigint,
+    data character varying
+);
+
+
+ALTER TABLE public.venda OWNER TO postgres;
+
+--
+-- TOC entry 208 (class 1259 OID 17133)
+-- Name: venda_produto; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.venda_produto (
+    venda_id integer NOT NULL,
+    produto_id integer NOT NULL,
+    "quantidade " integer
+);
+
+
+ALTER TABLE public.venda_produto OWNER TO postgres;
+
+--
+-- TOC entry 2900 (class 2606 OID 17137)
+-- Name: produto Produto_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.produto
+    ADD CONSTRAINT "Produto_pkey" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2888 (class 2606 OID 17139)
+-- Name: cliente cliente_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cliente
+    ADD CONSTRAINT cliente_pkey PRIMARY KEY (cpf);
+
+
+--
+-- TOC entry 2890 (class 2606 OID 17141)
+-- Name: estoque estoque_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.estoque
+    ADD CONSTRAINT estoque_pkey PRIMARY KEY (filial_id, produto_id);
+
+
+--
+-- TOC entry 2892 (class 2606 OID 17143)
+-- Name: filiais filiais_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.filiais
+    ADD CONSTRAINT filiais_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2894 (class 2606 OID 17145)
+-- Name: fornece fornece_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.fornece
+    ADD CONSTRAINT fornece_pkey PRIMARY KEY (filial_id, fornecedor_id);
+
+
+--
+-- TOC entry 2896 (class 2606 OID 17147)
+-- Name: fornecedor fornecedor_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.fornecedor
+    ADD CONSTRAINT fornecedor_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2898 (class 2606 OID 17149)
+-- Name: funcionario funcionario_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.funcionario
+    ADD CONSTRAINT funcionario_pkey PRIMARY KEY (cpf);
+
+
+--
+-- TOC entry 2902 (class 2606 OID 17151)
+-- Name: venda venda_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venda
+    ADD CONSTRAINT venda_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2904 (class 2606 OID 17153)
+-- Name: venda_produto venda_produto_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venda_produto
+    ADD CONSTRAINT venda_produto_pkey PRIMARY KEY (venda_id, produto_id);
+
+
+--
+-- TOC entry 2905 (class 2606 OID 17154)
+-- Name: estoque estoque_filial_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.estoque
+    ADD CONSTRAINT estoque_filial_id_fkey FOREIGN KEY (filial_id) REFERENCES public.filiais(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- TOC entry 2906 (class 2606 OID 17159)
+-- Name: estoque estoque_produto_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.estoque
+    ADD CONSTRAINT estoque_produto_id_fkey FOREIGN KEY (produto_id) REFERENCES public.produto(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- TOC entry 2907 (class 2606 OID 17164)
+-- Name: fornece fornece_filial_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.fornece
+    ADD CONSTRAINT fornece_filial_id_fkey FOREIGN KEY (filial_id) REFERENCES public.filiais(id) ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+--
+-- TOC entry 2908 (class 2606 OID 17169)
+-- Name: fornece fornece_fornecedor_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.fornece
+    ADD CONSTRAINT fornece_fornecedor_id_fkey FOREIGN KEY (fornecedor_id) REFERENCES public.fornecedor(id) ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+--
+-- TOC entry 2909 (class 2606 OID 17174)
+-- Name: produto produto_fornecedor_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.produto
+    ADD CONSTRAINT produto_fornecedor_id FOREIGN KEY (fornecedor_id) REFERENCES public.fornecedor(id) ON UPDATE CASCADE ON DELETE SET NULL NOT VALID;
+
+
+--
+-- TOC entry 2910 (class 2606 OID 17179)
+-- Name: venda venda_cliente_cpf_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venda
+    ADD CONSTRAINT venda_cliente_cpf_fkey FOREIGN KEY (cliente_cpf) REFERENCES public.cliente(cpf) ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+--
+-- TOC entry 2911 (class 2606 OID 17184)
+-- Name: venda venda_filial_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venda
+    ADD CONSTRAINT venda_filial_id_fkey FOREIGN KEY (filial_id) REFERENCES public.filiais(id) ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+--
+-- TOC entry 2912 (class 2606 OID 17189)
+-- Name: venda venda_funcionario_cpf_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venda
+    ADD CONSTRAINT venda_funcionario_cpf_fkey FOREIGN KEY (funcionario_cpf) REFERENCES public.funcionario(cpf) ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+--
+-- TOC entry 2914 (class 2606 OID 17204)
+-- Name: venda_produto venda_produto_produto_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venda_produto
+    ADD CONSTRAINT venda_produto_produto_id_fkey FOREIGN KEY (produto_id) REFERENCES public.produto(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+
+--
+-- TOC entry 2913 (class 2606 OID 17199)
+-- Name: venda_produto venda_produto_venda_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venda_produto
+    ADD CONSTRAINT venda_produto_venda_id_fkey FOREIGN KEY (venda_id) REFERENCES public.venda(id) ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+-- Completed on 2021-08-29 00:22:47
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ backupDB/venda_colchoes_instancias.sql
@@ -0,0 +1,167 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.3
+-- Dumped by pg_dump version 13.3
+
+-- Started on 2021-08-29 00:25:55
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 3045 (class 0 OID 17088)
+-- Dependencies: 200
+-- Data for Name: cliente; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.cliente VALUES (12345678901, 'Rian Victor', 'rianvictor@gmail.com', '123 Pindoretama');
+INSERT INTO public.cliente VALUES (12345688901, 'Rauane Batista', 'rauane@gmail.com', '23 Iguape');
+INSERT INTO public.cliente VALUES (15345688901, 'Samuel Afonso', 'samuel@gmail.com', '43 Eusébio');
+INSERT INTO public.cliente VALUES (42345688991, 'João da Silva', 'joazinho@gmail.com', '43 Rua X');
+INSERT INTO public.cliente VALUES (62345688901, 'Maria Souza', 'mariazinha@gmail.com', '22 Rua Y');
+INSERT INTO public.cliente VALUES (32349988901, 'Maria Souza', 'mariazinha@gmail.com', '4980 Gerald Plaza');
+INSERT INTO public.cliente VALUES (34545688901, 'Brenda Costa', 'brendacosta@gmail.com', '1234 Forster Drive');
+INSERT INTO public.cliente VALUES (99945688901, 'Evelyn Santos', 'evelyyn@gmail.com', '34 Forster Drive');
+INSERT INTO public.cliente VALUES (11231231236, 'Irene Rocha', 'ireenee@gmail.com', '473 Nova Avenue');
+INSERT INTO public.cliente VALUES (91236631236, 'Diana Silva', 'diaaana@gmail.com', '755 Nova Avenue');
+
+
+--
+-- TOC entry 3047 (class 0 OID 17097)
+-- Dependencies: 202
+-- Data for Name: filiais; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.filiais VALUES (10, '92 Londonderry Trail', 'filial1@gmail.com', 'Loja1');
+INSERT INTO public.filiais VALUES (11, '91210 Farwell Place', 'filial2@gmail.com', 'Loja2');
+INSERT INTO public.filiais VALUES (12, '63 Stoughton Alley', 'filial3@gmail.com', 'Loja3');
+INSERT INTO public.filiais VALUES (13, '090 Garrison Road', 'filial4@gmail.com', 'Loja4');
+
+
+--
+-- TOC entry 3049 (class 0 OID 17109)
+-- Dependencies: 204
+-- Data for Name: fornecedor; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.fornecedor VALUES (1, 85, 988888888, 'ortobom@gmail.com', '2450 São Gerardo');
+INSERT INTO public.fornecedor VALUES (2, 85, 988898888, 'herval@gmail.com', '370 Vila Mariana');
+INSERT INTO public.fornecedor VALUES (3, 85, 676698888, 'simmons@gmail.com', '749 Aldeota');
+
+
+--
+-- TOC entry 3051 (class 0 OID 17121)
+-- Dependencies: 206
+-- Data for Name: produto; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.produto VALUES (1, 'Boston', 'Ortobom', 'Mola', 1, '1,38 x 1,88 m', 1000);
+INSERT INTO public.produto VALUES (2, 'New York', 'Ortobom', 'Espuma', 1, '1,38 x 1,88 m', 2000);
+INSERT INTO public.produto VALUES (3, 'Orleans', 'Ortobom', 'Espuma', 1, '1,93 x 2,03 m', 3000);
+INSERT INTO public.produto VALUES (4, 'Miami', 'Herval', 'Mola', 2, '1,58 x 1,98 m', 2500);
+INSERT INTO public.produto VALUES (5, 'San Diego', 'Herval', 'Espuma', 2, '1,38 x 1,88 m', 1400);
+INSERT INTO public.produto VALUES (6, 'Orlando', 'Simmons', 'Espuma', 3, '1,93 x 2,03 m', 4000);
+INSERT INTO public.produto VALUES (7, 'Chicago', 'Simmons', 'Mola', 3, '1,93 x 2,03 m', 5000);
+
+
+
+--
+-- TOC entry 3046 (class 0 OID 17094)
+-- Dependencies: 201
+-- Data for Name: estoque; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.estoque VALUES (10, 1, 25);
+INSERT INTO public.estoque VALUES (11, 3, 90);
+INSERT INTO public.estoque VALUES (11, 4, 18);
+INSERT INTO public.estoque VALUES (12, 5, 24);
+INSERT INTO public.estoque VALUES (12, 6, 22);
+INSERT INTO public.estoque VALUES (13, 7, 10);
+INSERT INTO public.estoque VALUES (13, 1, 30);
+INSERT INTO public.estoque VALUES (10, 2, 14);
+INSERT INTO public.estoque VALUES (11, 2, 70);
+INSERT INTO public.estoque VALUES (13, 2, 29);
+
+
+--
+-- TOC entry 3048 (class 0 OID 17103)
+-- Dependencies: 203
+-- Data for Name: fornece; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.fornece VALUES (10, 1, 39, '1/07/21');
+INSERT INTO public.fornece VALUES (11, 1, 160, '1/07/21');
+INSERT INTO public.fornece VALUES (11, 2, 18, '1/07/21');
+INSERT INTO public.fornece VALUES (12, 2, 24, '1/07/21');
+INSERT INTO public.fornece VALUES (12, 3, 22, '1/07/21');
+INSERT INTO public.fornece VALUES (13, 1, 59, '1/07/21');
+INSERT INTO public.fornece VALUES (13, 3, 22, '1/07/21');
+
+
+--
+-- TOC entry 3050 (class 0 OID 17115)
+-- Dependencies: 205
+-- Data for Name: funcionario; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.funcionario VALUES (12625678906, 'Caio Barreros', 'Gerente', 7458);
+INSERT INTO public.funcionario VALUES (22628878906, 'Ester Lara', 'Caixa', 4780);
+INSERT INTO public.funcionario VALUES (22627878906, 'Nara Lima', 'Caixa', 4780);
+INSERT INTO public.funcionario VALUES (55429978906, 'Denise Damaceno', 'Vendedor', 1502);
+INSERT INTO public.funcionario VALUES (55429988906, 'Ricardo Silva', 'Vendedor', 1502);
+INSERT INTO public.funcionario VALUES (66429988906, 'Raissa Rocha', 'Vendedor', 1502);
+INSERT INTO public.funcionario VALUES (12688678906, 'Caio Barreros', 'Marketing', 9458);
+
+
+--
+-- TOC entry 3052 (class 0 OID 17127)
+-- Dependencies: 207
+-- Data for Name: venda; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.venda VALUES (1, 10, 12345678901, 55429978906, '19/10/21');
+INSERT INTO public.venda VALUES (2, 10, 12345688901, 55429978906, '19/10/21');
+INSERT INTO public.venda VALUES (3, 10, 42345688991, 55429978906, '21/10/21');
+INSERT INTO public.venda VALUES (4, 10, 15345688901, 55429978906, '26/10/21');
+INSERT INTO public.venda VALUES (5, 10, 91236631236, 55429978906, '25/10/21');
+INSERT INTO public.venda VALUES (6, 11, 62345688901, 55429988906, '19/10/21');
+INSERT INTO public.venda VALUES (7, 11, 32349988901, 55429988906, '19/10/21');
+INSERT INTO public.venda VALUES (8, 12, 34545688901, 66429988906, '11/07/21');
+INSERT INTO public.venda VALUES (9, 12, 99945688901, 66429988906, '30/09/21');
+INSERT INTO public.venda VALUES (10, 12, 11231231236, 66429988906, '25/08/21');
+
+
+--
+-- TOC entry 3053 (class 0 OID 17133)
+-- Dependencies: 208
+-- Data for Name: venda_produto; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.venda_produto VALUES (1, 1, 2);
+INSERT INTO public.venda_produto VALUES (4, 1, 2);
+INSERT INTO public.venda_produto VALUES (5, 1, 1);
+INSERT INTO public.venda_produto VALUES (6, 4, 1);
+INSERT INTO public.venda_produto VALUES (7, 4, 2);
+INSERT INTO public.venda_produto VALUES (8, 5, 3);
+INSERT INTO public.venda_produto VALUES (9, 6, 2);
+INSERT INTO public.venda_produto VALUES (10, 5, 1);
+INSERT INTO public.venda_produto VALUES (3, 2, 4);
+INSERT INTO public.venda_produto VALUES (2, 2, 1);
+
+
+-- Completed on 2021-08-29 00:25:55
+
+--
+-- PostgreSQL database dump complete
+--
+
